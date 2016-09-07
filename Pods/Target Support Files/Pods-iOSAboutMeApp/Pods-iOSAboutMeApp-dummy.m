95b106ab2800b61d44d86437c9111b48f0ced31f
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-09-08 01:38:33 +0700

    pod install for walkthrough
    pod 'BWWalkthrough'

---
+++ Pods/Target Support Files/Pods-iOSAboutMeApp/Pods-iOSAboutMeApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSAboutMeApp : NSObject
+@end
+@implementation PodsDummy_Pods_iOSAboutMeApp
+@end
---
+++ iOSAboutMeApp/WalkthroughViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WalkthroughViewController.swift
+//  iOSAboutMeApp
+//
+//  Created by Chotipat on 9/8/2559 BE.
+//  Copyright © 2559 Chotipat. All rights reserved.
+//
+
+import Foundation
