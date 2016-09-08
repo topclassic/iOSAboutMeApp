2599a7f403440d8b596d38d1290154e905ba3f8c
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-09-08 18:17:05 +0700

    Change color tableview

--- iOSAboutMeApp/SWRevealTableViewController.swift
+++ iOSAboutMeApp/SWRevealTableViewController.swift
@@ -10,4 +10,14 @@ import Foundation
 
 class SWRevealTableViewController: UITableViewController {
     
+    // change color
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.backgroundColor = UIColor.lightGrayColor()
+    }
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
+    // end change color
+    
 }
